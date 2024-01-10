04c8e3fcd3d9705285e2ed353ab0251b0f5fa4fa
chizhang <andreachizhang@yahoo.com>
2024-01-10 17:42:11 +0100

    pca

--- lab/code/lab_pca.R
+++ lab/code/lab_pca.R
@@ -1,4 +1,130 @@
-# code for PCA
+# code for EDA and PCA
+
+# food  -----
+
+food <- read.table('./lab/data/Food.txt', header=T)
+head(food)
+# pulses: legume
+colnames(food)[7] <- 'Legume'
+
+## explore and viz ----
+# let us have a look at the variables
+dim(food)
+
+colnames(food)
+rownames(food)
+
+# subsetting
+food[rownames(food) == 'Norway',]
+food[rownames(food) %in% c('Norway', 'Denmark', 'Sweden'),]
+
+# explore one variable
+mean(food$Fish)
+max(food$Fish)
+min(food$Fish)
+
+## visualize ----
+# par(mfrow = c(1, 1))
+# reset margin
+# par(mar = c(5.1, 4.1, 4.1, 2.1))
+
+hist(food$Fish)
+hist(food$Fish, breaks = 20)
+
+hist(food$Meat)
+
+
+# boxplot
+boxplot(food, las = 2,
+        main = 'Food consumption across European countries')
+
+
+# two variables
+plot(food$Fish, food$Meat)
+
+# add country
+text(food$Fish, food$Meat, labels = rownames(food))
+
+# choose aother pair of food
+plot(food$Cereals, food$Fruit)
+text(food$Cereals, food$Fruit, labels = rownames(food))
+
+# pair-wise scatter
+plot(food)
+
+
+## PCA ----
+
+pc_food <- prcomp(food, scale=TRUE)
+# pc_food
+summary(pc_food)
+
+
+
+## loadings
+# (coefficients in the linear combination of the variables
+# that defines each component)
+loading_food <- pc_food$rotation
+loading_food
+round(loading_food, digits = 2)
+
+## scores
+# (projections of each datum in the new space defined by the components)
+# (if we select some of the components, dimensional reduction of the data)
+scores_food <- pc_food$x
+scores_food
+
+
+
+## proportion of explained variance
+pc_food_var <- pc_food$sdev^2
+pc_food_pve <- pc_food_var/sum(pc_food_var)
+pc_food_pve
+cumsum(pc_food_pve)
+
+
+## Some PCA visualisation
+# par(mfrow = c(1,1))
+
+# biplot
+biplot(pc_food)
+# biplot(pc_food, choices = c(1,2)) # first two pc
+
+biplot(pc_food, choices = c(1,3)) # pc1, pc3
+
+
+
+# graphical summary of the proportion of variance explained
+
+# variance of the PCs
+barplot(pc_food_var, las=2,
+        main='Principal components', ylab='Variances',
+        names.arg = paste('PC ',1:length(pc_food_var),sep=''))
+
+# variance of the original data variables
+barplot(apply(food, 2, sd)^2, las=2, main='Original Variables', ylim=c(0,150), ylab='Variances')
+
+
+# PVE
+plot(cumsum(pc_food_pve), type='b', axes=F, xlab='number of components',
+     ylab='contribution to total variance', ylim=c(0,1))
+abline(h=1, col='blue')
+abline(h=0.8, lty=2, col='blue')
+box()
+axis(2,at=0:10/10,labels=0:10/10)
+axis(1,at=1:ncol(food),labels=1:ncol(food),las=2)
+
+
+
+
+## OPTIONAL
+# loadings and PC variances are respectively
+# eigenvectors and eigenvalues of the data correlation matrix
+pc.food$rotation
+pc.food$rotation[1:p,1:p]
+pc.food.var
+eigen(cor(food))
+
 
 
 # NCI60  ----
@@ -127,106 +253,5 @@ legend('topleft', col=rainbow(length(unique(groups))), legend=paste('group ',uni
 
 ## The first component is evidently *very* important: this we see in both plots
 ## (PVE and the PC1 vs PC2 and PC3). We also see that PC1 describes the groups.
-
-
-
-
-
-
-
-# food example (if need) -----
-
-
-
-food <- read.table('./lab/data/Food.txt', header=T)
-head(food)
-n <- dim(food)[1]
-p <- dim(food)[2]
-n
-p
-
-# let us have a look at the variables
-
-boxplot(food, main = 'Food consumption across European countries')
-# quite different variability of the variables -> I scale the data in the PCA
-
-## PCA
-
-pc.food <- prcomp(food, scale=TRUE)
-pc.food
-summary(pc.food)
-
-## let us explore the PCA results
-
-## proportion of explained variance (same as above!)
-pc.food.var <- pc.food$sdev^2
-pc.food.pve <- pc.food.var/sum(pc.food.var)
-pc.food.pve
-cumsum(pc.food.pve)
-
-## loadings
-# (coefficients in the linear combination of the variables
-#  that defines each component)
-load.food    <- pc.food$rotation
-load.food
-
-
-## scores
-# (projections of each datum in the new space defined by the components)
-# (if we select some of the components, dimensional reduction of the data)
-scores.food <- pc.food$x
-scores.food
-
-## SIDE NOTE:
-# loadings and PC variances are respectively
-# eigenvectors and eigenvalues of the data correlation matrix
-pc.food$rotation
-pc.food$rotation[1:p,1:p]
-pc.food.var
-eigen(cor(food))
-
-
-## Some PCA visualisation
-
-## 1. graphical summary of the proportion of variance explained
-layout(matrix(c(2,3,1,3),2,byrow=T))
-# variance of the PCs
-barplot(pc.food.var, las=2, main='Principal components', ylab='Variances',
-        names.arg = paste('PC ',1:length(pc.food.var),sep=''))
-# variance of the original data variables
-barplot(apply(food, 2, sd)^2, las=2, main='Original Variables', ylim=c(0,150), ylab='Variances')
-# PVE
-plot(cumsum(pc.food.pve), type='b', axes=F, xlab='number of components',
-     ylab='contribution to total variance', ylim=c(0,1))
-abline(h=1, col='blue')
-abline(h=0.8, lty=2, col='blue')
-box()
-axis(2,at=0:10/10,labels=0:10/10)
-axis(1,at=1:ncol(food),labels=1:ncol(food),las=2)
-
-
-## 2. dispersion on original data and in scores
-layout(matrix(c(1,2),2))
-boxplot(food, las=2, col='red', main='Original Variables')
-scores.food <- data.frame(scores.food)
-boxplot(scores.food, las=2, col='red', main='Principal components')
-
-## 3. correlation among scores compared to original variables
-pairs(food, asp=1)
-pairs(scores.food, asp=1)
-
-
-## 4. loadings of the first 3 PCs (those that might be the relevant ones)
-par(mar = c(1,4,0,2), mfrow = c(3,1))
-for(i in 1:3) barplot(load.food[,i], ylim = c(-1, 1))
-
-## 5. a possible comparison: scatterplot of the data corresponding to the 2 variables
-##                           with larger variance, and of the scores of the first
-##                           2 principal components
-layout(matrix(c(1,2),1))
-plot(food[,'Cereals'],food[,'Milk'],type="n",xlab="Cereals",ylab="Milk", asp=1)
-text(food[,'Cereals'],food[,'Milk'],dimnames(food)[[1]], cex=.7)
-plot(scores.food[,1],-scores.food[,2],type="n",xlab="pc1",ylab="-pc2", asp=1)
-text(scores.food[,1],-scores.food[,2],dimnames(food)[[1]], cex=.7)
 
 
